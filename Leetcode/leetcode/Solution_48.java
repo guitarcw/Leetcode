9bfd3bac0910c94aebeb20244288e49286980f3c
weic <cw_bupt@163.com>
2017-09-24 22:23:45 +0800

    一开始的想法思路是对的，只是旋转方向未考虑周全。重点：在于坐标变化的规律。

---
+++ Leetcode/leetcode/Solution_48.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class Solution_48 {
+  public void rotate(int[][] matrix) {
+        int len=matrix.length;
+        for(int i=0;i<len/2;i++) {
+        	for(int j=i;j<len-i-1;j++) {
+        		int temp=matrix[i][j];
+        		matrix[i][j]=matrix[len-j-1][i];
+        		matrix[len-j-1][i]=matrix[len-i-1][len-j-1];
+        		matrix[len-i-1][len-j-1]=matrix[j][len-i-1];
+        		matrix[j][len-i-1]=temp;
+        	}
+        }
+        
+    }
+
+}
