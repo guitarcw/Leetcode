bf09ade69c4949cbaf4e1b3282e9d8bf5d8309b0
weic <cw_bupt@163.com>
2017-09-24 17:57:52 +0800

    技巧：每个位置判断不出现相同的已经出现过的数值。结合回溯法

--- Leetcode/leetcode/Solution_47.java
+++ Leetcode/leetcode/Solution_47.java
@@ -1,6 +1,7 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution_47 {
@@ -8,19 +9,25 @@ public class Solution_47 {
 		List<List<Integer>> list=new ArrayList<>();
 		List<Integer> temp=new ArrayList<>();
 		if(nums.length==0||nums==null) return list;
+		Arrays.sort(nums);
 		get(list, temp, nums, 0);
 		return list;
 	        
 	    }
 	public void get(List<List<Integer>> list,List<Integer> temp,int[] nums,int start) {
-		if(temp.size()==nums.length) list.add(new ArrayList<>(temp));
-		else for (int i = start; i < nums.length; i++) {
-			if(i>start&&nums[i]==nums[i-1]) continue;
+		if(temp.size()==nums.length) {list.add(new ArrayList<>(temp)); return;}
+		else {
+			List<Integer> com=new ArrayList<>();//ÅĞ¶ÏÎ»ÖÃÊÇ·ñÖØ¸´
+			for (int i = start; i < nums.length; i++) {
+			if(i>start&&com.contains(nums[i])) continue;
+			com.add(nums[i]);
 			swap(nums, start, i);
 			temp.add(nums[start]);
 			get(list, temp, nums, start+1);
 			temp.remove(temp.size()-1);
-			swap(nums, start, i);
+			swap(nums, start, i);		
+		}
+			
 		}
 	}
 	private void swap(int[] nums,int i,int j) {
