228af2c0ac287e0c815626ecbfb2165c4cda6187
weic <cw_bupt@163.com>
2017-10-01 22:47:52 +0800

    test class

---
+++ Leetcode/test/test.java
@@ -0,0 +1,16 @@
+package test;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=0;
+		int m=n++;
+		int[][] k=new int[1][1];
+		System.out.print(m);
+		System.out.print(n);
+		System.out.print(k[0][0]);
+
+	}
+
+}
