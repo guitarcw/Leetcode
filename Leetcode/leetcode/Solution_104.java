90fdc1b798a1344a3524aea5e2d5dd160fc07e9a
weic <cw_bupt@163.com>
2017-10-24 21:34:19 +0800

    o

--- Leetcode/leetcode/Solution_104.java
+++ Leetcode/leetcode/Solution_104.java
@@ -1,5 +1,11 @@
 package leetcode;
-
+//Maximum Depth of Binary Tree
 public class Solution_104 {
+public int maxDepth(TreeNode root) {
+	if(root==null)
+		return 0;
+	else return Math.max(1+maxDepth(root.left),1+maxDepth(root.right));
+        
+    }
 
 }
