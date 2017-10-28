40d8ad7f23c9cea707dc6c980a3453296d8d5ffb
weic <cw_bupt@163.com>
2017-10-28 22:15:40 +0800

    叶节点

--- Leetcode/leetcode/Solution_112.java
+++ Leetcode/leetcode/Solution_112.java
@@ -1,7 +1,7 @@
 package leetcode;
 //Path Sum
 public class Solution_112 {
-	public boolean hasPathSum( TreeNode root ,int sum) {
+	public boolean hasPathSum( TreeNode root ,int sum) {//难点：需要判断的是到达的是叶节点
 		 if(root==null) return false;
 		 if(root.val==sum&&root.left==null&&root.right==null) return true;
 		 return hasPathSum(root.left, sum-root.val)||hasPathSum(root.right, sum-root.val);
