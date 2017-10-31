e46b73b06bbacd2f30481ea89c9cf4be0db2a509
weic <cw_bupt@163.com>
2017-10-31 10:23:37 +0800

    okok

--- Leetcode/leetcode/Solution_125.java
+++ Leetcode/leetcode/Solution_125.java
@@ -5,19 +5,20 @@ public class Solution_125 {
 		if(s.length()<=0) return true;
 		int start=0;int end=s.length()-1;
 		while(start<=end) {
-			while(start<(s.length()-1)&&!isChar(s.charAt(start))) start++;
-			while(end>0&&!isChar(s.charAt(end))) end--;
-			if(Character.toLowerCase(s.charAt(start))==Character.toLowerCase(s.charAt(end))) {
-				start++;end++;
+			while(start<(s.length()-1)&&!Character.isLetterOrDigit(s.charAt(start))) start++;
+			while(end>=0&&!Character.isLetterOrDigit(s.charAt(end))) end--;
+			if(start>end) return true;
+			else if(Character.toLowerCase(s.charAt(start))==Character.toLowerCase(s.charAt(end))) { 
+				end--;start++;
 				continue;
 			}
 			else return false;
 		}
 		return true;
 	}
-	private boolean isChar(char ch) {
+	/*private boolean isChar(char ch) {
 		if('a'<=ch&&ch<='Z') return true;
 		else return false;
-	}
+	}*///判断问题，出现错误，主要对Java 字母与Ascii码值对应理解不清楚 。a-z：97-122 A-Z：65-90 0-9：48-57
 
 }
