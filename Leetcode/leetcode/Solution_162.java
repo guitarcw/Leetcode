ec8d3c262bef0e94b95f94a3e564d156a6b83b4a
weic <cw_bupt@163.com>
2017-12-21 21:02:57 +0800

    okok

---
+++ Leetcode/leetcode/Solution_162.java
@@ -0,0 +1,18 @@
+package leetcode;
+//162.Find Peak Element
+public class Solution_162 {
+	public int findPeakElement(int[] nums) {
+		int start=0;
+		int end=nums.length-1;
+		while(start<end) {
+			int mid=(start+end)/2;
+			if(nums[mid]<nums[mid+1])
+				start=mid+1;
+			else end=mid;
+		}
+		return start;
+		
+	}
+	
+
+}
